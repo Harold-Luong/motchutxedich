4a25a5c2791805f7c87a12e6aa9427fcfe2d71f8
Duc Trong <ductrong16051998tn@gmail.com>
2024-08-19 00:07:05 +0700

    Add getUserById from store

--- src/App.js
+++ src/App.js
@@ -1,8 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import { getUserById } from "./fireStore/user/getUserById";
 
 function App() {
-  return <div className="App">Iint project</div>;
+  const userId = "HSXg366yGD1aEl8PDwkA";
+  const [user, setUser] = useState({});
+  //get user from local storage
+  const getUserFromLocalStorage = () => {
+    return JSON.parse(localStorage.getItem("userData")) || {};
+  };
+
+  // set user to local storage
+  const setUserToLocalStorage = (userData) => {
+    if (JSON.parse(localStorage.getItem("userData"))) {
+      console.log("User already exists in local storage.");
+      return;
+    }
+    localStorage.setItem("userData", JSON.stringify(userData));
+  };
+
+  //get user from firestore
+  const fetchUserData = async () => {
+    const userData = await getUserById(userId);
+    if (userData) {
+      setUser(userData);
+    }
+    return userData;
+  };
+
+  // useEffects function fetches user data from firestore
+  useEffect(() => {
+    let userData = getUserFromLocalStorage();
+    if (Object.keys(userData).length === 0) {
+      fetchUserData()
+        .then((userData) => {
+          console.log(userData);
+          setUser(userData);
+          setUserToLocalStorage(userData);
+        })
+        .catch((error) => {
+          console.error("Error fetching data:", error);
+        });
+    }
+  }, []);
+
+  return <div className="App">{user.name}</div>;
 }
 
 export default App;
