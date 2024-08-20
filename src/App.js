fb04c9ca624a47ef5ecd85f4a63ae00e1c930f7f
ER_TrongLuong <trong.luong@ever-rise.co.jp>
2024-08-20 17:20:54 +0700

    Add map travel

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from "react";
 import "./App.css";
+import "./component/map/MapTravel"
+import ProvincesCheckBox from "./component/map/ProvincesCheckBox";
 
 function App() {
-  return <div className="App">Iint project</div>;
+  return <div className="App">
+    <ProvincesCheckBox/>
+  </div>;
 }
 
 export default App;
