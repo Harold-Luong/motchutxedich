8ab524d9fdfd01d3052c64541453fb52702de8d6
Duc Trong <ductrong16051998tn@gmail.com>
2024-10-24 20:23:39 +0700

    Divide components according to each province

---
+++ src/component/map/map-province/QuanDaoHoangSa.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+const QuanDaoHoangSa = () => {
+  return (
+    <>
+      <text
+        transform="matrix(0.9994 -0.0332 0.0332 0.9994 368.9939 192.4707)"
+        fill="#ffffff"
+        fontFamily="'MyriadPro-Regular'"
+        fontSize="4.7062"
+      >
+        QUẦN ĐẢO HOÀNG SA
+      </text>
+
+      <text
+        transform="matrix(0.9994 -0.0332 0.0332 0.9994 368.9939 192.4707)"
+        fill="none"
+        stroke="black"
+        strokeWidth="0.5"
+        fontFamily="'MyriadPro-Regular'"
+        fontSize="4.7062"
+      >
+        QUẦN ĐẢO HOÀNG SA
+      </text>
+
+      <text
+        transform="matrix(0.9994 -0.0332 0.0332 0.9994 381.405 197.7095)"
+        fill="#CDCCCC"
+        fontFamily="'MyriadPro-Regular'"
+        fontSize="4.7062"
+      >
+        (Việt Nam)
+      </text>
+
+      <text
+        transform="matrix(0.9994 -0.0332 0.0332 0.9994 381.405 197.7095)"
+        fill="none"
+        stroke="black"
+        strokeWidth="0.5"
+        fontFamily="'MyriadPro-Regular'"
+        fontSize="4.7062"
+      >
+        (Việt Nam)
+      </text>
+    </>
+  );
+};
+
+export default QuanDaoHoangSa;
